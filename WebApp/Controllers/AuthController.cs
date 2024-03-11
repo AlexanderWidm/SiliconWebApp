b04b3a616156aacc23384bf41a2eca1e013ddfb1
AlexanderWidm <widamark.alexander@gmail.com>
2024-03-11 15:31:51 +0100

    sign up form created

---
+++ WebApp/Controllers/AuthController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers;
+
+public class AuthController : Controller
+{
+
+    #region SignUp
+
+    [HttpGet]
+    [Route("/signup")]
+    public IActionResult SignUp()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [Route("/signup")]
+    //Den här måste ha en model i sig, i detta fall SignUpViewModel
+    public IActionResult SignUp(SignUpViewModel viewModel)
+    {
+        //gör något if valid (spara i databas, redirect user)
+        if (ModelState.IsValid)
+        {
+
+        }
+        //annars returnera detta
+        return View(viewModel);
+    }
+
+    #endregion
+
+    #region SignIn
+    [HttpGet]
+    [Route("/signin")]
+    public IActionResult SignIn()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [Route("/signin")]
+    //Den här måste ha en model i sig, i detta fall SignUpViewModel
+    public IActionResult SignIn(SignUpViewModel viewModel)
+    {
+        //gör något if valid (spara i databas, redirect user)
+        if (ModelState.IsValid)
+        {
+
+        }
+        //annars returnera detta
+        return View(viewModel);
+    }
+    #endregion
+}
--- WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f078feafb59ba5675422c5e6c3c2d6132aef9e03")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+425ef089c04471295d8298d7e58d69a5aeae19b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
