c3a66502465621a9f35b44c4acffc1ea2802d391
Alexander Station <widmark.alexander@gmail.com>
2024-03-13 20:21:10 +0100

    made more responsive and added device section

--- WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+425ef089c04471295d8298d7e58d69a5aeae19b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b04b3a616156aacc23384bf41a2eca1e013ddfb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
