6b92744386e3f95991f7401ac2b6f32a4d218f6a
Alexander Station <widmark.alexander@gmail.com>
2024-04-04 17:58:30 +0200

    fixed dark mode cookies

---
+++ WebApp/Controllers/SiteSettingsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers;
+
+public class SiteSettingsController : Controller
+{
+    public IActionResult Theme(string mode)
+    {
+        var option = new CookieOptions
+        {
+            Expires = DateTime.Now.AddYears(1)
+        };
+        Response.Cookies.Append("theme", mode, option);
+        return Ok();
+    }
+}
